c3d1301da0a3cfbe90a9f2f3ef15bb99411d6df3
Fitamp <tylerhansenth@gmail.com>
2013-06-29 16:40:19 -0700

    add source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Tue Oct 2 16:58:31 KST 2012"
+#define LINUX_COMPILE_BY "kisuk.choi"
+#define LINUX_COMPILE_HOST "GT-01"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
